c2c88f28bf96a18514570efb40c3acca38baf626
Mugouren <67725595+Mugouren@users.noreply.github.com>
2020-07-03 18:06:52 +0800

    RealRefraction

--- optProgram/Elements/OptSystem.cs
+++ optProgram/Elements/OptSystem.cs
@@ -57,5 +57,48 @@ namespace optProgram.elements
 
             return GaussianRefraction(incidentBeam2);
         }
+
+
+        public Beam RealRefraction(Beam incidentBeam1) //Calculate exit beam using recursion
+        {
+
+            double u1p, l1p, u2, l2;
+            double incidentAngle, exitAngle;
+            double radius = RadiusQ.Dequeue();
+            double n = RefractiveIndexQ.Dequeue();
+            double np = RefractiveIndexQ.Peek(); //Do not delete np item for later use: n2 = n1p
+            // Math.Asin(1) = 1.5707963267948966 Math.Sin(Math.PI) = 0
+            // using rad not deg
+            double sinI = (incidentBeam1.l - radius) * Math.Sin(incidentBeam1.u) / radius;
+            if(Math.Abs(sinI)>1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sinI), "入射光线超半球！");
+            }
+            incidentAngle = Math.Asin(sinI);
+
+            double sinIp = n * sinI / np;
+            if (Math.Abs(sinIp) > 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(sinIp), "发生全反射！");
+            }
+            exitAngle = Math.Asin(sinIp);
+
+            IncidentAngleQ.Enqueue(incidentAngle);
+            ExitAngleQ.Enqueue(exitAngle);
+
+            u1p = incidentAngle + incidentBeam1.u - exitAngle;
+            l1p = radius + radius * sinIp / Math.Sin(u1p);
+
+            Beam exitBeam1 = new Beam(l1p, u1p);
+            if (RadiusQ.Count == 0) return exitBeam1; // the last sphere does not have any sphere behind
+
+            u2 = u1p;
+            l2 = l1p - IntervalQ.Dequeue();
+            Beam incidentBeam2 = new Beam(l2, u2);
+
+            return RealRefraction(incidentBeam2);
+        }
+
+
     }
 }
