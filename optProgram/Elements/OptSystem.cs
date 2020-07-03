c4452e2d9dc53331b6256e0587e73578f8e3fe89
2377671184 <sjq2377671184@gmail.com>
2020-07-03 17:29:05 +0800

    添加近轴无穷远光线

--- optProgram/Elements/OptSystem.cs
+++ optProgram/Elements/OptSystem.cs
@@ -13,9 +13,11 @@ namespace optProgram.elements
         Queue<double> IntervalQ = new Queue<double>();
         Queue<double> IncidentAngleQ = new Queue<double>();
         Queue<double> ExitAngleQ = new Queue<double>();
-        public OptSystem(Queue<Sphere> systemdata, Obj obj)
+        double pupilDiameter;
+        public OptSystem(Queue<Sphere> systemdata, Obj obj,double pupilD)
         {
             int Qlength = systemdata.Count;
+            this.pupilDiameter = pupilD;
             RefractiveIndexQ.Enqueue(obj.envRefractive);
             int inputCount = systemdata.Count;
             while (systemdata.Count > 0)
@@ -24,27 +26,31 @@ namespace optProgram.elements
                 Sphere tmp = systemdata.Dequeue();
                 RadiusQ.Enqueue(tmp.r);
                 RefractiveIndexQ.Enqueue(tmp.n);
-                if(inputCount!=1)
+                if(inputCount!=1&&systemdata.Count!=0)
                     IntervalQ.Enqueue(tmp.d);
 
             }
-            if (inputCount != 1)
-                RefractiveIndexQ.Enqueue(obj.envRefractive);
+            
         }
-        public Beam GaussianRefraction(Beam incidentBeam1) //Calculate exit beam using recursion
+        public Beam GaussianRefraction(Beam incidentBeam1,bool isinf) //Calculate exit beam using recursion
         {
-
             double u1p, l1p, u2, l2;
             double incidentAngle, exitAngle;
             double radius = RadiusQ.Dequeue();
             double n = RefractiveIndexQ.Dequeue();
             double np = RefractiveIndexQ.Peek(); //Do not delete np item for later use: n2 = n1p
-
-            incidentAngle = (incidentBeam1.l - radius) * incidentBeam1.u / radius;
+            if (isinf)
+            {
+                incidentAngle = pupilDiameter / 2 / radius;
+                isinf = !isinf;
+            }
+            else
+            {
+                incidentAngle = (incidentBeam1.l - radius) * incidentBeam1.u / radius;
+            }
             exitAngle = n * incidentAngle / np;
             IncidentAngleQ.Enqueue(incidentAngle);
             ExitAngleQ.Enqueue(exitAngle);
-
             u1p = incidentAngle + incidentBeam1.u - exitAngle;
             l1p = radius + radius * exitAngle / u1p;
 
@@ -55,7 +61,7 @@ namespace optProgram.elements
             l2 = l1p - IntervalQ.Dequeue();
             Beam incidentBeam2 = new Beam(l2, u2);
 
-            return GaussianRefraction(incidentBeam2);
+            return GaussianRefraction(incidentBeam2,isinf);
         }
     }
 }
