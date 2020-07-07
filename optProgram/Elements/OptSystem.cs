9800157b2ffe23257417c8f9928a533be5fc052b
2377671184 <sjq2377671184@gmail.com>
2020-07-07 20:14:41 +0800

    彗差修复

--- optProgram/Elements/OptSystem.cs
+++ optProgram/Elements/OptSystem.cs
@@ -88,13 +88,7 @@ namespace optProgram.elements
             Dictionary<string, Beam> incidentBeamRealOn;
             Dictionary<string, Beam> incidentBeamRealOff;
 
-            if (isInfinite)
-                incident = new astigBeam(0, obj.fieldAngle, Math.Pow(-10, 15), Math.Pow(-10, 15), 0);
-            else
-                incident = new astigBeam(0, Math.Atan(obj.objHeight / obj.objDistance),
-                    Math.Sqrt(obj.objHeight * obj.objHeight + obj.objDistance * obj.objDistance),
-                    Math.Sqrt(obj.objHeight * obj.objHeight + obj.objDistance * obj.objDistance),
-                    0);
+
 
             RadiusOff = new Queue<double>(RadiusOn);
             IntervalOff = new Queue<double>(IntervalOn);
@@ -108,7 +102,6 @@ namespace optProgram.elements
 
             incidentBeamGaussianOn = initOnaxialGaussian();
             incidentBeamRealOn = initOnAxialReal(K2);
-
 
             foreach (KeyValuePair<string, Queue<double>> kvp in RIndexOn)
             {
@@ -179,6 +172,34 @@ namespace optProgram.elements
 
 
             // Output Image Height, Tangential Coma and Distortion Data(Transversal)------complete
+
+            if (isInfinite)
+                incident = new astigBeam(0, obj.fieldAngle, Math.Pow(-10, 15), Math.Pow(-10, 15), 0);
+            else
+            {
+
+                /*double PA, x, r, u;
+                r = RadiusOff.Peek();
+                u = Math.Atan(obj.pupilDiameter / 2 / obj.objDistance);
+                double incidentAngle = Math.Asin((obj.objDistance - r) * Math.Sin(u) / r);
+                PA = (obj.objDistance * Math.Sin(u)) / Math.Cos(0.5 * (incidentAngle - u));
+                x = 0.5 * PA * PA / r;
+                incident = new astigBeam(obj.objDistance, u,
+                      (obj.objDistance - x) / Math.Cos(u),
+                      (obj.objDistance - x) / Math.Cos(u),
+                      x);*/
+                double PA, x, r, u;
+                r = RadiusOff.Peek();
+                u = Math.Atan(obj.objHeight / obj.objDistance);
+                double incidentAngle = Math.Asin((obj.objDistance - r) * Math.Sin(u) / r);
+                PA = (obj.objDistance * Math.Sin(u)) / Math.Cos(0.5 * (incidentAngle - u));
+                x = 0.5 * PA * PA / r;
+                incident = new astigBeam(obj.objDistance, u,
+                      Math.Sqrt(obj.objHeight * obj.objHeight + obj.objDistance * obj.objDistance),
+                      Math.Sqrt(obj.objHeight * obj.objHeight + obj.objDistance * obj.objDistance),
+                      0);
+            }
+
             astigBeam output = new astigBeam(0, 0, 0, 0, 0);
             output = ImageDiffRefraction(incident, new Queue<double>(RadiusOff), new Queue<double>(RIndexOff["d"]), new Queue<double>(IntervalOff));
             double ltp = output.t * Math.Cos(output.u) + output.x;
@@ -213,7 +234,7 @@ namespace optProgram.elements
             relativeD1 = absoluteD1 / y0["1  d"] * 100;
             absoluteD7 = yp["0.7  0  d"] - y0["0.7  d"];
             relativeD7 = absoluteD7 / y0["0.7  d"] * 100;
-            if(!isInfinite)
+            if (!isInfinite)
             {
                 absoluteD1 = -absoluteD1;
                 absoluteD7 = -absoluteD7;
@@ -403,7 +424,7 @@ namespace optProgram.elements
                     else
                     {
                         double tanU1;
-                        tanU1 = (K1 * obj.objHeight - K2 * pupilDiameter / 2) / obj.objDistance;
+                        tanU1 = (K1 * obj.objHeight - K2 * pupilDiameter / 2) / -obj.objDistance;
                         beam.Add(K1.ToString() + "  " + K2.ToString(), new Beam(K2 * pupilDiameter / 2 / tanU1,
                             Math.Atan(tanU1)));
                     }
@@ -559,33 +580,32 @@ namespace optProgram.elements
         private astigBeam ImageDiffRefraction(astigBeam incidentBeam1, Queue<double> Radius,
              Queue<double> RefractiveIndex, Queue<double> Interval)
         {
-            double s1p, t1p, s2, t2, PA1, X1, PA2, X2, D1;
+            double s1p, t1p, s2, t2, PA2, X2, D1;
             double incidentAngle, exitAngle;
             double radius = Radius.Dequeue();
 
             double n = RefractiveIndex.Dequeue();
             double np = RefractiveIndex.Peek();
-
             incidentAngle = Math.Asin((incidentBeam1.l - radius) * Math.Sin(incidentBeam1.u) / radius);
             exitAngle = Math.Asin(n * Math.Sin(incidentAngle) / np);
-            //IncidentAngleQ.Enqueue(incidentAngle);
-            //ExitAngleQ.Enqueue(exitAngle);
+            //incidentAngle = (incidentBeam1.l - radius) * incidentBeam1.u / radius;
+            //exitAngle = n * incidentAngle / np;
             s1p = np / ((np * Math.Cos(exitAngle) - n * Math.Cos(incidentAngle)) / radius + n / incidentBeam1.s);
             t1p = np * Math.Cos(exitAngle) * Math.Cos(exitAngle) / ((np * Math.Cos(exitAngle) -
                 n * Math.Cos(incidentAngle)) / radius + n * Math.Cos(incidentAngle) * Math.Cos(incidentAngle) / incidentBeam1.t);
             double u1p = incidentAngle + incidentBeam1.u - exitAngle;
             double l1p = radius + radius * Math.Sin(exitAngle) / Math.Sin(u1p);
+            //double l1p = radius + radius * exitAngle / u1p;
             astigBeam exitBeam1 = new astigBeam(l1p, u1p, s1p, t1p, incidentBeam1.x);
             if (Radius.Count == 0) return exitBeam1; // the last sphere does not have any sphere behind
             double radius2 = Radius.Peek();
             double u2 = u1p;
             double l2 = l1p - Interval.Peek();
             double Inci2 = Math.Asin((l2 - radius2) * Math.Sin(u2) / radius2);
-            PA1 = incidentBeam1.l * Math.Sin(incidentBeam1.u) / Math.Cos(0.5 * (incidentAngle - incidentBeam1.u));
-            X1 = PA1 * PA1 / (2 * radius);
+
             PA2 = l2 * Math.Sin(u2) / Math.Cos(0.5 * (Inci2 - u2));
             X2 = PA2 * PA2 / (2 * radius2);
-            D1 = (Interval.Dequeue() - X1 + X2) / Math.Cos(u1p);
+            D1 = (Interval.Dequeue() - incidentBeam1.x + X2) / Math.Cos(u1p);
             t2 = t1p - D1;
             s2 = s1p - D1;
             astigBeam incidentBeam2 = new astigBeam(l2, u2, s2, t2, X2);
